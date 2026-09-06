98d4bfef1cf804a33cc1e1e5b151883ae74ac5f7
Your Name <you@example.com>
2026-09-06 13:03:21 +0000

    -

--- src/http/ngd_http.h
+++ src/http/ngd_http.h
@@ -17,6 +17,33 @@ struct ngd_http_header_t {
 };
 //
 struct ngd_http_t {
+    int state;
+    int state_req;
+    //
+    ngd_pool_t *pool;
+    ngd_pool_t *pool_req;
+    //
+    ngd_buf_t headbuf;
+    ngd_buf_t bodybuf;
+    ngd_buf_t outbuf;
+    //reqline
+    str_t smethod;
+    str_t suri;
+    str_t sver;
+    u_char *method_start;
+    u_char *method_end;
+    u_char *uri_start;
+    u_char *uri_end;
+    u_char *ver_start;
+    u_char *ver_end;
+    //headers
+    ngd_list_t headers;
+    u_char *key_start;
+    u_char *key_end;
+    u_char *value_start;
+    u_char *value_end;
+    //
+    size_t content_length;
 
 }
 //
